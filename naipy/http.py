4322b3be7be9de5269cb1a6d9bc857e44d2793de
VoidAsMad <103942316+VoidAsMad@users.noreply.github.com>
2022-08-15 10:27:48 +0900

    #49 비동기 지원 `AsyncNaipyRequest` 클래스 추가

--- naipy/http.py
+++ naipy/http.py
@@ -1,8 +1,10 @@
 import urllib.request
 import warnings
-from typing import List
+from typing import List, Dict
 import json
 from naipy.error import ConverError
+import aiohttp
+
 
 class NaipyRequest:
   base_url = "https://openapi.naver.com/v1/"
@@ -43,3 +45,43 @@ class NaipyRequest:
 
   def get_result(self, tag: List[str], text):
     return self.request(tag, text)
+
+class AsyncNaipyRequest:
+  base_url = "https://openapi.naver.com/v1/"
+
+  def __init__(
+    self, 
+    client_id : str = None, 
+    client_secret : str = None
+  ) -> None:
+    if not client_id or not client_id:
+      self.client_id = "p7aYK48ehm6TqdhTt_yv"
+      self.client_secret = "UJAqwrxSfQ"
+      warnings.warn("샘플키로 요청합니다.", UserWarning)
+    else:
+      self.client_id = client_id
+      self.client_secret = client_secret
+
+
+  async def request(
+    self,
+    tag : List[str],
+    text : str
+  ):
+    headers : Dict = {"X-Naver-Client-Id" : self.client_id ,
+                      "X-Naver-Client-Secret" : self.client_secret}
+    encText = urllib.parse.quote(text)
+    URL = self.base_url + tag[0] + "/" + tag[1] + "?query=" + encText
+    async with aiohttp.ClientSession(headers = headers) as session:
+      async with session.get(URL) as response:
+        rescode = response.status
+        
+        if(rescode==200):
+          result = await response.json()
+          return result
+
+        else:
+          raise ConverError(f"Error Code : {rescode}")
+
+  async def get_result(self, tag: List[str], text):
+    return await self.request(tag, text)
