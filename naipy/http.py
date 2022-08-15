aaefd75b2e58372901d66f544f0718c0e442363b
VoidAsMad <103942316+VoidAsMad@users.noreply.github.com>
2022-08-15 16:50:33 +0900

    Fix: #104

--- naipy/http.py
+++ naipy/http.py
@@ -14,7 +14,7 @@ class NaipyRequest:
     client_id : str = None, 
     client_secret : str = None
   ) -> None:
-    if not client_id or not client_id:
+    if not client_id or not client_secret:
       self.client_id = "p7aYK48ehm6TqdhTt_yv"
       self.client_secret = "UJAqwrxSfQ"
       warnings.warn("샘플키로 요청합니다.", UserWarning)
@@ -22,8 +22,7 @@ class NaipyRequest:
       self.client_id = client_id
       self.client_secret = client_secret
 
-      
-  def request(
+  def s_request(
     self,
     tag : List[str],
     text : str
@@ -40,11 +39,32 @@ class NaipyRequest:
       result = json.loads(response_body)
       return result
 
+  def t_request(
+    self,
+    tag : List[str],
+    text : str
+  ):
+    encText = urllib.parse.quote(text)
+    URL = self.base_url + tag[0] + "/" + tag[1]
+    
+    data = tag[2] + encText
+    request = urllib.request.Request(URL)
+    request.add_header("X-Naver-Client-Id", self.client_id)
+    request.add_header("X-Naver-Client-Secret", self.client_secret)
+    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+    rescode = response.getcode()
+    if(rescode==200):
+      response_body = response.read()
+      result = json.loads(response_body)
+      return result
     else:
       raise ConverError(f"Error Code : {rescode}")  
 
   def get_result(self, tag: List[str], text):
-    return self.request(tag, text)
+    if tag[0] == "search":
+      return self.s_request(tag, text)
+    else:
+      return self.t_request(tag, text)
 
 class AsyncNaipyRequest:
   base_url = "https://openapi.naver.com/v1/"
@@ -54,34 +74,56 @@ class AsyncNaipyRequest:
     client_id : str = None, 
     client_secret : str = None
   ) -> None:
-    if not client_id or not client_id:
+    if not client_id or not client_secret:
       self.client_id = "p7aYK48ehm6TqdhTt_yv"
       self.client_secret = "UJAqwrxSfQ"
+      warnings.warn("샘플키로 요청합니다.", UserWarning)
       warnings.warn("샘플키로 요청합니다.", UserWarning)
     else:
       self.client_id = client_id
       self.client_secret = client_secret
 
-
-  async def request(
+  async def s_request(
     self,
     tag : List[str],
     text : str
   ):
-    headers : Dict = {"X-Naver-Client-Id" : self.client_id ,
+    headers : Dict = {"X-Naver-Client-Id" : self.client_id,
                       "X-Naver-Client-Secret" : self.client_secret}
     encText = urllib.parse.quote(text)
     URL = self.base_url + tag[0] + "/" + tag[1] + "?query=" + encText
     async with aiohttp.ClientSession(headers = headers) as session:
       async with session.get(URL) as response:
         rescode = response.status
-        
         if(rescode==200):
           result = await response.json()
           return result
+        else:
+          raise ConverError(f"Error Code : {rescode}")
 
+  async def t_request(
+    self,
+    tag : List[str],
+    text : str
+  ):
+    headers : Dict = {"X-Naver-Client-Id" : self.client_id,
+                      "X-Naver-Client-Secret" : self.client_secret,
+                      "Content-Type" : "application/x-www-form-urlencoded"
+                     }
+    encText = urllib.parse.quote(text)
+    URL = self.base_url + tag[0] + "/" + tag[1]
+    params = tag[2] + encText
+    async with aiohttp.ClientSession(headers = headers) as session:
+      async with session.post(URL, data = params.encode('utf-8')) as response:
+        rescode = response.status
+        if(rescode==200):
+          result = await response.json()
+          return result
         else:
           raise ConverError(f"Error Code : {rescode}")
 
   async def get_result(self, tag: List[str], text):
-    return await self.request(tag, text)
+    if tag[0] == "search":
+      return await self.s_request(tag, text)
+    else:
+      return await self.t_request(tag, text)
