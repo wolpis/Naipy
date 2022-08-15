b4cc038c36d66b1c27ff0879cfbadce6e975984b
VoidAsMad <103942316+VoidAsMad@users.noreply.github.com>
2022-08-15 16:48:16 +0900

    #87 Feat: 번역기능 추가

--- naipy/async_client.py
+++ naipy/async_client.py
@@ -83,3 +83,41 @@ class Search(AsyncNaipyRequest):
     data = await self.get_result(tag, text)
     data["type"] = str(current_func_name)
     return DocNaipy(data = data, **data)
+
+class Translation(AsyncNaipyRequest):
+  """
+  네이버 검색 라이브러리입니다.<br>(Parameters가 빈칸일시 샘플키로 요청합니다.)
+  #### client_id
+  [네이버개발자센터](https://developers.naver.com/main/)에서 발급받은 `Client ID`를 입력합니다.
+  #### client_secret
+  [네이버개발자센터](https://developers.naver.com/main/)에서 발급받은 `Client Secret`를 입력합니다.
+  """
+  def __init__(
+    self, 
+    client_id : str = None, 
+    client_secret : str = None
+  ) -> None:
+    super().__init__(
+      client_id = client_id,
+      client_secret = client_secret
+    )
+
+  async def detect(self, text : str) -> DetectNaipy:
+    """
+    언어를 인식합니다. [언어코드표](https://developers.naver.com/docs/papago/papago-nmt-api-reference.md#%ED%8C%8C%EB%9D%BC%EB%AF%B8%ED%84%B0)
+    """
+    current_func_name = sys._getframe().f_code.co_name
+    tag = ['papago', 'detectLangs', 'query=']
+    data = await self.get_result(tag, text)
+    data["type"] = str(current_func_name)
+    return DetectNaipy(data, **data)
+    
+  async def translation(self, text : str, target : str) -> N2mtNaipy:
+    """
+    문장또는 단어를 번역합니다. `target`파라미터는 [언어코드표](https://developers.naver.com/docs/papago/papago-nmt-api-reference.md#%ED%8C%8C%EB%9D%BC%EB%AF%B8%ED%84%B0)를 참고해주세요!
+    """
+    source = await self.detect(text).langCode
+    tag = ['papago', 'n2mt', f'source={source}&target={target}&text=']
+    data = await self.get_result(tag, text)
+    data = data['message']['result']
+    return N2mtNaipy(data , **data)
