0909104964ef89fd5cf10f354301787e665bf999
Wolpis Carter <103942316+VoidAsMad@users.noreply.github.com>
2022-09-01 17:03:13 +0900

    REFACTOR

--- naipy/http.py
+++ naipy/http.py
@@ -1,14 +1,11 @@
-import urllib.request
 import warnings
-from typing import List, Dict
-import json
-from naipy.error import ConverError
+from typing import Dict, Union
+from naipy.error import HTTPException
 import aiohttp
-
 
 class NaipyRequest:
   base_url = "https://openapi.naver.com/v1/"
-  
+
   def __init__(
     self, 
     client_id : str = None, 
@@ -22,108 +19,28 @@ class NaipyRequest:
       self.client_id = client_id
       self.client_secret = client_secret
 
-  def s_request(
+  async def request(
     self,
-    tag : List[str],
-    text : str
-  ):
-    encText = urllib.parse.quote(text)
-    URL = self.base_url + tag[0] + "/" + tag[1] + "?query=" + encText
-    request = urllib.request.Request(URL)
-    request.add_header("X-Naver-Client-Id", self.client_id)
-    request.add_header("X-Naver-Client-Secret", self.client_secret)
-    response = urllib.request.urlopen(request)
-    rescode = response.getcode()
-    if(rescode==200):
-      response_body = response.read()
-      result = json.loads(response_body)
-      return result
-
-  def t_request(
-    self,
-    tag : List[str],
-    text : str
-  ):
-    encText = urllib.parse.quote(text)
-    URL = self.base_url + tag[0] + "/" + tag[1]
-    
-    data = tag[2] + encText
-    request = urllib.request.Request(URL)
-    request.add_header("X-Naver-Client-Id", self.client_id)
-    request.add_header("X-Naver-Client-Secret", self.client_secret)
-    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
-    rescode = response.getcode()
-    if(rescode==200):
-      response_body = response.read()
-      result = json.loads(response_body)
-      return result
-    else:
-      raise ConverError(f"Error Code : {rescode}")  
-
-  def get_result(self, tag: List[str], text):
-    if tag[0] == "search":
-      return self.s_request(tag, text)
-    else:
-      return self.t_request(tag, text)
-
-class AsyncNaipyRequest:
-  base_url = "https://openapi.naver.com/v1/"
-
-  def __init__(
-    self, 
-    client_id : str = None, 
-    client_secret : str = None
-  ) -> None:
-    if not client_id or not client_secret:
-      self.client_id = "p7aYK48ehm6TqdhTt_yv"
-      self.client_secret = "UJAqwrxSfQ"
-      warnings.warn("샘플키로 요청합니다.", UserWarning)
-      warnings.warn("샘플키로 요청합니다.", UserWarning)
-    else:
-      self.client_id = client_id
-      self.client_secret = client_secret
-
-  async def s_request(
-    self,
-    tag : List[str],
-    text : str
-  ):
-    headers : Dict = {"X-Naver-Client-Id" : self.client_id,
-                      "X-Naver-Client-Secret" : self.client_secret}
-    encText = urllib.parse.quote(text)
-    URL = self.base_url + tag[0] + "/" + tag[1] + "?query=" + encText
+    method : str,
+    endpoint : str,
+    params: Dict[str, Union[str, int]]
+  ) -> Dict[str, Union[str, int]]:
+    headers = {
+      "X-Naver-Client-Id" : self.client_id,
+      "X-Naver-Client-Secret" : self.client_secret,
+      "Content-Type" : "application/x-www-form-urlencoded"
+    }
+    url = self.base_url + endpoint
     async with aiohttp.ClientSession(headers = headers) as session:
-      async with session.get(URL) as response:
+      async with session.request(method, url=url, headers=headers, params=params) as response:
         rescode = response.status
         if(rescode==200):
-          result = await response.json()
-          return result
+          return await response.json()
         else:
-          raise ConverError(f"Error Code : {rescode}")
+          raise HTTPException(f"Error Code : {rescode}")
 
-  async def t_request(
-    self,
-    tag : List[str],
-    text : str
-  ):
-    headers : Dict = {"X-Naver-Client-Id" : self.client_id,
-                      "X-Naver-Client-Secret" : self.client_secret,
-                      "Content-Type" : "application/x-www-form-urlencoded"
-                     }
-    encText = urllib.parse.quote(text)
-    URL = self.base_url + tag[0] + "/" + tag[1]
-    params = tag[2] + encText
-    async with aiohttp.ClientSession(headers = headers) as session:
-      async with session.post(URL, data = params.encode('utf-8')) as response:
-        rescode = response.status
-        if(rescode==200):
-          result = await response.json()
-          return result
-        else:
-          raise ConverError(f"Error Code : {rescode}")
+  async def get_search(self, tag: str, params: Dict[str, Union[str, int]]):
+    return await self.request("GET", f"search/{tag}", params)
 
-  async def get_result(self, tag: List[str], text):
-    if tag[0] == "search":
-      return await self.s_request(tag, text)
-    else:
-      return await self.t_request(tag, text)
+  async def get_translation(self, tag: str, params: Dict[str, Union[str, int]]):
+    return await self.request("POST", f"papago/{tag}", params)
