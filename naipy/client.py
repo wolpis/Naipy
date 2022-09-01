bd22f52d467ce47e2eb2b8cadf8c2aefd297e94d
Wolpis Carter <103942316+VoidAsMad@users.noreply.github.com>
2022-09-01 17:00:44 +0900

    REFACTOR

--- naipy/client.py
+++ naipy/client.py
@@ -1,9 +1,8 @@
-from naipy.http import AsyncNaipyRequest
+from naipy.http import NaipyRequest
 from naipy.model import ImageNaipy, BlogNaipy, BookNaipy, EncycNaipy, CafearticleNaipy, KinNaipy, WebkrNaipy, ShopNaipy, DocNaipy, DetectNaipy, N2mtNaipy
-import sys
 import random
 
-class Search(AsyncNaipyRequest):
+class Search(NaipyRequest):
   """
   네이버 비동기 검색 라이브러리입니다.<br>(Parameters가 빈칸일시 샘플키로 요청합니다.)
   #### client_id
@@ -21,15 +20,13 @@ class Search(AsyncNaipyRequest):
       client_id = client_id,
       client_secret = client_secret
     )
-    
+
   async def image(self, text : str) -> ImageNaipy:
     """
     이미지를 검색합니다.
     """
-    current_func_name = sys._getframe().f_code.co_name
-    tag = ['search', str(current_func_name)]
-    data = await self.get_result(tag, text)
-    data["type"] = str(current_func_name)
+    params = {"query" : text}
+    data = await self.get_search('image', params=params)
     data["item"] = random.choice(data['items'])
     return ImageNaipy(data = data, **data)
 
@@ -37,10 +34,8 @@ class Search(AsyncNaipyRequest):
     """
     블로그를 검색합니다.
     """
-    current_func_name = sys._getframe().f_code.co_name
-    tag = ['search', str(current_func_name)]
-    data = await self.get_result(tag, text)
-    data["type"] = str(current_func_name)
+    params = {"query" : text}
+    data = await self.get_search('blog', params=params)
     data["item"] = random.choice(data['items'])
     return BlogNaipy(data = data, **data)
 
@@ -48,10 +43,8 @@ class Search(AsyncNaipyRequest):
     """
     도서를 검색합니다.
     """
-    current_func_name = sys._getframe().f_code.co_name
-    tag = ['search', str(current_func_name)]
-    data = await self.get_result(tag, text)
-    data["type"] = str(current_func_name)
+    params = {"query" : text}
+    data = await self.get_search('book', params=params)
     data["item"] = random.choice(data['items'])
     return BookNaipy(data = data, **data)
 
@@ -59,10 +52,8 @@ class Search(AsyncNaipyRequest):
     """
     백과사전를 검색합니다.
     """
-    current_func_name = sys._getframe().f_code.co_name
-    tag = ['search', str(current_func_name)]
-    data = await self.get_result(tag, text)
-    data["type"] = str(current_func_name)
+    params = {"query" : text}
+    data = await self.get_search('encyc', params=params)
     data["item"] = random.choice(data['items'])
     return EncycNaipy(data = data, **data)
 
@@ -70,10 +61,8 @@ class Search(AsyncNaipyRequest):
     """
     카페글를 검색합니다.
     """
-    current_func_name = sys._getframe().f_code.co_name
-    tag = ['search', str(current_func_name)]
-    data = await self.get_result(tag, text)
-    data["type"] = str(current_func_name)
+    params = {"query" : text}
+    data = await self.get_search('cafearticle', params=params)
     data["item"] = random.choice(data['items'])
     return CafearticleNaipy(data = data, **data)
 
@@ -81,10 +70,8 @@ class Search(AsyncNaipyRequest):
     """
     네이버 지식인을 검색합니다.
     """
-    current_func_name = sys._getframe().f_code.co_name
-    tag = ['search', str(current_func_name)]
-    data = await self.get_result(tag, text)
-    data["type"] = str(current_func_name)
+    params = {"query" : text}
+    data = await self.get_search('kin', params=params)
     data["item"] = random.choice(data['items'])
     return KinNaipy(data = data, **data)
 
@@ -92,10 +79,8 @@ class Search(AsyncNaipyRequest):
     """
     웹사이트를 검색합니다.
     """
-    current_func_name = sys._getframe().f_code.co_name
-    tag = ['search', str(current_func_name)]
-    data = await self.get_result(tag, text)
-    data["type"] = str(current_func_name)
+    params = {"query" : text}
+    data = await self.get_search('webkr', params=params)
     data["item"] = random.choice(data['items'])
     return WebkrNaipy(data = data, **data)
 
@@ -103,10 +88,8 @@ class Search(AsyncNaipyRequest):
     """
     쇼핑을 검색합니다.
     """
-    current_func_name = sys._getframe().f_code.co_name
-    tag = ['search', str(current_func_name)]
-    data = await self.get_result(tag, text)
-    data["type"] = str(current_func_name)
+    params = {"query" : text}
+    data = await self.get_search('shop', params=params)
     data["item"] = random.choice(data['items'])
     return ShopNaipy(data = data, **data)
 
@@ -114,16 +97,14 @@ class Search(AsyncNaipyRequest):
     """
     전문자료를 검색합니다.
     """
-    current_func_name = sys._getframe().f_code.co_name
-    tag = ['search', str(current_func_name)]
-    data = await self.get_result(tag, text)
-    data["type"] = str(current_func_name)
+    params = {"query" : text}
+    data = await self.get_search('doc', params=params)
     data["item"] = random.choice(data['items'])
     return DocNaipy(data = data, **data)
 
-class Translation(AsyncNaipyRequest):
+class Translation(NaipyRequest):
   """
-  네이버 비동기 번역 라이브러리입니다.<br>(Parameters가 빈칸일시 샘플키로 요청합니다.)
+  네이버 번역 라이브러리입니다.<br>(Parameters가 빈칸일시 샘플키로 요청합니다.)
   #### client_id
   [네이버개발자센터](https://developers.naver.com/main/)에서 발급받은 `Client ID`를 입력합니다.
   #### client_secret
@@ -143,19 +124,21 @@ class Translation(AsyncNaipyRequest):
     """
     언어를 인식합니다. [언어코드표](https://developers.naver.com/docs/papago/papago-nmt-api-reference.md#%ED%8C%8C%EB%9D%BC%EB%AF%B8%ED%84%B0)
     """
-    current_func_name = sys._getframe().f_code.co_name
-    tag = ['papago', 'detectLangs', 'query=']
-    data = await self.get_result(tag, text)
-    data["type"] = str(current_func_name)
-    return DetectNaipy(data, **data)
-    
+    params = {"query" : text}
+    data = await self.get_translation('detectLangs', params=params)
+    return DetectNaipy(data = data, **data)
+
   async def translation(self, text : str, target : str) -> N2mtNaipy:
     """
     문장또는 단어를 번역합니다. `target`파라미터는 [언어코드표](https://developers.naver.com/docs/papago/papago-nmt-api-reference.md#%ED%8C%8C%EB%9D%BC%EB%AF%B8%ED%84%B0)를 참고해주세요!
     """
     source = await self.detect(text)
     source = source.langCode
-    tag = ['papago', 'n2mt', f'source={source}&target={target}&text=']
-    data = await self.get_result(tag, text)
+    params = {
+      "text" : text,
+      "source" : source,
+      "target" : target
+    }
+    data = await self.get_translation('n2mt', params=params)
     data = data['message']['result']
     return N2mtNaipy(data , **data)
